1da307e48091ae11a8c82c8986eb482b4a97bdd9
vaz_mf <vazfernandam@gmail.com>
2021-05-17 09:29:34 -0300

    Implementando a tela de produtos e itens

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class Product {
   final String id;
-  final String titile;
+  final String title;
   final String description;
   final double price;
   final String imageUrl;
@@ -10,7 +10,7 @@ class Product {
 
   Product({
     @required this.id,
-    @required this.titile,
+    @required this.title,
     @required this.description,
     @required this.price,
     @required this.imageUrl,

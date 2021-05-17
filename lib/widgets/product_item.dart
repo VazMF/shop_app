42b4cd8b834ad71ae3486535c9849b698fc8453b
vaz_mf <vazfernandam@gmail.com>
2021-05-17 11:37:34 -0300

    Add navegaçao entre telas

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -9,14 +11,22 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTileBar(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName, 
+              arguments: id
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
             icon: Icon(Icons.favorite),

56fdbca7b6f3166535b9d35bfce834a83d2be0f5
vaz_mf <vazfernandam@gmail.com>
2021-05-18 08:57:14 -0300

    Alternativa a sintaxe do provider

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -13,8 +13,8 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (ctx, index) => ChangeNotifierProvider(
-        create: (ctx) => products[index],
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(
           // products[index].id,
           // products[index].title,

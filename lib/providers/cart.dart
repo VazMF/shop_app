66c7bd0ca6259cb671d993413abf17011f15cc3e
vaz_mf <vazfernandam@gmail.com>
2021-05-19 09:09:15 -0300

    Adicionando opçao remover do carrinho com snakbar

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -37,30 +37,47 @@ class Cart with ChangeNotifier {
   void addItem(String productId, double price, String title) {
     if (_items.containsKey(productId)) {
       _items.update(
-        productId, 
-        (existingCarItem) => CartItem(
-          id: existingCarItem.id,
-          title: existingCarItem.title,
-          price: existingCarItem.price,
-          quantity: existingCarItem.quantity + 1,
-        )
-      );
+          productId,
+          (existingCarItem) => CartItem(
+                id: existingCarItem.id,
+                title: existingCarItem.title,
+                price: existingCarItem.price,
+                quantity: existingCarItem.quantity + 1,
+              ));
     } else {
       _items.putIfAbsent(
-        productId,
-        () => CartItem(
-          id: DateTime.now().toString(),
-          title: title,
-          price: price,
-          quantity: 1,
-        )
-      );
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
     }
     notifyListeners();
   }
 
   void removeItem(String productId) {
     _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                quantity: existingCartItem.quantity - 1,
+                price: existingCartItem.price,
+              ));
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 

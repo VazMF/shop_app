d599fe9ea3fdc2e86bc0a78114553aeb88cc5880
vaz_mf <vazfernandam@gmail.com>
2021-05-19 10:09:34 -0300

    Adicionando pagina de produtos

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/orders.dart';
-import 'package:shop_app/screens/orders_screen.dart';
+import './providers/orders.dart';
+import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 import './providers/cart.dart';
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/product_overview_screen.dart';
 import './providers/products_provider.dart';
-import './screens/product_detail_screen.dart';
-import './screens/orders_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,6 +41,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+import '../providers/products_provider.dart';
+import '../widgets/user_product_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<ProductsProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+            itemCount: productData.items.length,
+            itemBuilder: (_, i) => Column(children: [
+                  UserProductItem(
+                    productData.items[i].title,
+                    productData.items[i].imageUrl,
+                  ),
+                  Divider(),
+                ])),
+      ),
+    );
+  }
+}

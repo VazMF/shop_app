1d012849b827106089194fab49e5bd3f6f4897d0
vaz_mf <vazfernandam@gmail.com>
2021-05-20 23:51:02 -0300

    Fetching de pedidos

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -30,6 +30,9 @@ class ProductsProvider with ChangeNotifier {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
       final List<Product> loadedProducts = [];
+      if (extractedData == null) {
+        return;
+      }
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
           id: prodId,
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,8 +5,32 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Orders>(context).fetchAndSetOrders().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +40,7 @@ class OrdersScreen extends StatelessWidget {
         title: Text('Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
+      body: _isLoading ? Center(child: CircularProgressIndicator()) : ListView.builder(
         itemCount: ordersData.orders.length,
         itemBuilder: (context, i) => OrderItem(ordersData.orders[i]),
       ),
